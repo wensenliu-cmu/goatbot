2408139300d6c1c5d8d72cbb241a27193dbbb065
Wensen Liu <wensenl@andrew.cmu.edu>
2025-03-23 18:09:35 -0400

    Fixed swapped identify_servo and dump_servo code

--- dump_servo_data.py
+++ dump_servo_data.py
@@ -4,4 +4,14 @@ import board_helper_functions as bhf
 if __name__ == '__main__':
     print(f"Dumping All Requested Hiwonder HTD-45H Bus Servo Data")
     helper = bhf.BoardHelper()
-    helper.identify_servo()
+
+    if len(sys.argv) > 1:
+        servos = sys.argv[1:]
+        for servo in servos:
+            servo_id = int(servo)
+            try:
+                helper.log_servo(servo_id=servo_id)
+            except Exception as e:
+                print(f"Servo {servo_id}:\tERROR: {e}")
+
+    else: print(f"ERROR: No servo ID given")
--- identify_servo.py
+++ identify_servo.py
@@ -4,14 +4,4 @@ import board_helper_functions as bhf
 if __name__ == '__main__':
     print(f"Identifying Attached Hiwonder HTD-45H Bus Servo")
     helper = bhf.BoardHelper()
-    
-    if len(sys.argv) > 1:
-        servos = sys.argv[1:]
-        for servo in servos:
-            servo_id = int(servo)
-            try:
-                helper.log_servo(servo_id=servo_id)
-            except Exception as e:
-                print(f"Servo {servo_id}:\tERROR: {e}")
-
-    else: print(f"ERROR: No servo ID given")
+    helper.identify_servo()
